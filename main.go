10190cb043fb4714897f81b97d85b3015d4750eb
André Luiz da Silva <andreluizmicro@gmail.com>
2022-05-04 19:27:11 -0300

    Add environmets variables, and config connection database

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"api/src/config"
 	"api/src/routes"
 	"fmt"
 	"log"
@@ -8,8 +9,12 @@ import (
 )
 
 func main() {
+	config.Bootstrap()
+
+	fmt.Println(config.ConnectionString)
+
 	fmt.Println("Rodando API")
 	r := routes.Generate()
 
-	log.Fatal(http.ListenAndServe(":9001", r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), r))
 }
